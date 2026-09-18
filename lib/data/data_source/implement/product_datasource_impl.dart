f04c506913b8f2fa63fcc7a80d4c74b921601601
maryamhaithem <maryamhaithem697@gmail.com>
2026-09-19 01:35:03 +0300

    Update login screen

--- lib/data/data_source/implement/product_datasource_impl.dart
+++ lib/data/data_source/implement/product_datasource_impl.dart
@@ -1,5 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:mm/core/network/error/failures.dart';
+import 'package:mm/core/network/api/api_consumer.dart';
+import 'package:mm/data/data_source/implement/product_datasource_impl.dart';
+
 class ProductDataSourceImpl implements ProductDataSource {
 
   final ApiConsumer apiConsumer;
@@ -8,19 +11,19 @@ class ProductDataSourceImpl implements ProductDataSource {
 
   @override
   Future<Either<Failure, Map<String, dynamic>>> getProducts() async {
-    final response = await apiConsumer.get('/products');
+    final response = await apiConsumer.get(path: '/products');
 
-    return Right(response);
+    return response;
   }
 
   @override
   Future<Either<Failure, Map<String, dynamic>>> getProductDetails({
     required String productId,
   }) async {
-    final response = await apiConsumer.get(
-      '/products/$productId',
-    );
+    final response = await
+    apiConsumer.get(path: '/products/$productId');
 
-    return Right(response);
+
+    return response;
   }
 }
