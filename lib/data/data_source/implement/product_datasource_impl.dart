6a69113ec921495092287972f21b9f1c66dc6399
maryamhaithem <maryamhaithem697@gmail.com>
2026-09-19 02:47:23 +0300

    Fix product datasource

--- lib/data/data_source/implement/product_datasource_impl.dart
+++ lib/data/data_source/implement/product_datasource_impl.dart
@@ -1,10 +1,9 @@
 import 'package:dartz/dartz.dart';
-import 'package:mm/core/network/error/failures.dart';
 import 'package:mm/core/network/api/api_consumer.dart';
-import 'package:mm/data/data_source/implement/product_datasource_impl.dart';
+import 'package:mm/core/network/error/failures.dart';
+import 'package:mm/data/data_source/abstract/product_datasource.dart';
 
 class ProductDataSourceImpl implements ProductDataSource {
-
   final ApiConsumer apiConsumer;
 
   ProductDataSourceImpl(this.apiConsumer);
@@ -13,17 +12,21 @@ class ProductDataSourceImpl implements ProductDataSource {
   Future<Either<Failure, Map<String, dynamic>>> getProducts() async {
     final response = await apiConsumer.get(path: '/products');
 
-    return response;
-  }
+    return response.fold(
+          (failure) => Left(failure),
+          (data) => Right(data),
+    );  }
 
   @override
   Future<Either<Failure, Map<String, dynamic>>> getProductDetails({
     required String productId,
   }) async {
-    final response = await
-    apiConsumer.get(path: '/products/$productId');
+    final response =
+    await apiConsumer.get(path: '/products/$productId');
 
-
-    return response;
+    return response.fold(
+          (failure) => Left(failure),
+          (data) => Right(data),
+    );
   }
 }
